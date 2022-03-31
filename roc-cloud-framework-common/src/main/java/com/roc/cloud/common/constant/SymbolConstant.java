2e16f3eb2f58684b01d621ec57a0f79b65048f6e
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 01:34:51 +0800

    Create SymbolConstant.java

---
+++ roc-cloud-framework-common/src/main/java/com/roc/cloud/common/constant/SymbolConstant.java
@@ -0,0 +1,29 @@
+package com.roc.cloud.common.constant;
+
+/**
+ * 常用符号 <br>
+ *
+ * @date: 2020/9/30 <br>
+ * @author: Roc <br>
+ * @since: 1.0 <br>
+ * @version: 1.0 <br>
+ */
+public interface SymbolConstant {
+
+    String WELL = "#";
+
+    String PERIOD_LOWER = ".";
+
+    String COMMA_LOWER = ",";
+
+    String COLON_LOWER = ":";
+
+    String SLASH_LOWER = "/";
+
+    String SLASH_SLASH_LOWER = SLASH_LOWER + SLASH_LOWER;
+
+    String BACK_SLASH_LOWER = "\\";
+
+    String BACK_SLASH_SLASH_LOWER = BACK_SLASH_LOWER + BACK_SLASH_LOWER;
+
+}
