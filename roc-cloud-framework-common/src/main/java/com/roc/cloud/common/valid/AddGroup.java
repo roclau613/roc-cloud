276ae42ad8caebeb71282e8d7939e801b98668ba
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 01:56:52 +0800

    Create AddGroup.java

---
+++ roc-cloud-framework-common/src/main/java/com/roc/cloud/common/valid/AddGroup.java
@@ -0,0 +1,13 @@
+package com.roc.cloud.common.valid;
+
+/**
+ * <br>
+ *
+ * @date: 2021/10/13 <br>
+ * @author: Roc <br>
+ * @since: 1.0 <br>
+ * @version: 1.0 <br>
+ */
+public interface AddGroup {
+
+}
