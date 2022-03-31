50f05ed83270dca470abc46a6d78fea0f4d7fdc0
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 01:56:30 +0800

    Create QueryGroup.java

---
+++ roc-cloud-framework-common/src/main/java/com/roc/cloud/common/valid/QueryGroup.java
@@ -0,0 +1,13 @@
+package com.roc.cloud.common.valid;
+
+/**
+ * <br>
+ *
+ * @date: 2022/02/13 <br>
+ * @author: Roc <br>
+ * @since: 1.0 <br>
+ * @version: 1.0 <br>
+ */
+public interface QueryGroup {
+
+}
