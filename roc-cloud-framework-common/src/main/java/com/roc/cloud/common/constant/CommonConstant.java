ec035cc368d378e1177ba4ec9ca92c5e7406ba74
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 00:38:32 +0800

    Create CommonConstant.java

---
+++ roc-cloud-framework-common/src/main/java/com/roc/cloud/common/constant/CommonConstant.java
@@ -0,0 +1,159 @@
+package com.roc.cloud.common.constant;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 全局公共常量
+ *
+ * @author: Frank
+ * @date 2018/10/29
+ */
+public class CommonConstant {
+    /**
+     * 项目版本号(banner使用)
+     */
+    public final static String PROJECT_VERSION = "1.0.0";
+
+    /**
+     * token请求头名称
+     */
+    public final static String TOKEN_HEADER = "Authorization";
+
+    /**
+     * The access token issued by the authorization server. This value is REQUIRED.
+     */
+    public final static String ACCESS_TOKEN = "access_token";
+
+    public final static String BEARER_TYPE = "Bearer";
+
+    /**
+     * 标签 header key
+     */
+    public final static String HEADER_LABEL = "x-label";
+
+    /**
+     * 标签 header 分隔符
+     */
+    public final static String HEADER_LABEL_SPLIT = ",";
+
+    /**
+     * 标签或 名称
+     */
+    public final static String LABEL_OR = "labelOr";
+
+    /**
+     * 标签且 名称
+     */
+    public final static String LABEL_AND = "labelAnd";
+
+    /**
+     * 权重key
+     */
+    public final static String WEIGHT_KEY = "weight";
+
+    /**
+     * 删除
+     */
+    public final static String STATUS_DEL = "1";
+
+    /**
+     * 正常
+     */
+    public final static String STATUS_NORMAL = "0";
+
+    /**
+     * 锁定
+     */
+    public final static String STATUS_LOCK = "9";
+
+    /**
+     * 目录
+     */
+    public final static Integer CATALOG = -1;
+
+    /**
+     * 菜单
+     */
+    public final static Integer MENU = 1;
+
+    /**
+     * 权限
+     */
+    public final static Integer PERMISSION = 2;
+
+    /**
+     * 删除标记
+     */
+    public final static String DEL_FLAG = "is_del";
+
+    /**
+     * 超级管理员用户名
+     */
+    public final static String ADMIN_USER_NAME = "admin";
+
+    /**
+     * 公共日期格式
+     */
+    public final static String MONTH_FORMAT = "yyyy-MM";
+    public final static String DATE_FORMAT = "yyyy-MM-dd";
+    public final static String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public final static String SIMPLE_MONTH_FORMAT = "yyyyMM";
+    public final static String SIMPLE_DATE_FORMAT = "yyyyMMdd";
+    public final static String SIMPLE_DATETIME_FORMAT = "yyyyMMddHHmmss";
+
+    public final static String DEF_USER_PASSWORD = "123456";
+
+    public final static String LOCK_KEY_PREFIX = "LOCK_KEY:";
+
+    /**
+     * 租户id参数
+     */
+    public final static String TENANT_ID_PARAM = "tenantId";
+
+
+    /**
+     * 日志链路追踪id信息头
+     */
+    public final static String TRACE_ID_HEADER = "x-traceId-header";
+
+    /**
+     * 日志链路追踪id日志标志
+     */
+    public final static String LOG_TRACE_ID = "traceId";
+
+    /**
+     * 日志链路追踪id日志标志-机构
+     */
+    public final static String LOG_COMPANY_ID = "companyId";
+
+    /**
+     * 日志链路追踪id日志标志-请求地址
+     */
+    public final static String LOG_REQUEST_URL = "requestUrl";
+    /**
+     * 负载均衡策略-版本号 信息头
+     */
+//    String Z_L_T_VERSION = "z-l-t-version";
+    /**
+     * 注册中心元数据 版本号
+     */
+    public final static String METADATA_VERSION = "version";
+
+
+    /**
+     * 请求IP
+     */
+//    public static final String ORIGINAL_FORWARDED_FOR = "X-original-Forwarded-For";
+
+    /**
+     * 入口IP
+     */
+    public static final String ORIGINAL_FORWARDED_HOST_FOR = "X-original-Forwarded-Host-For";
+
+    /**
+     * 灰度环境
+     */
+    public static final List<String> PROFILES = Arrays.asList("dev");
+}
