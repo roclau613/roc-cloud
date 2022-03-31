585d29a5ee00fc5c4c321beab7d26e9374d5b6fa
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 01:24:23 +0800

    Create ConfigConstants.java

---
+++ roc-cloud-framework-common/src/main/java/com/roc/cloud/common/constant/ConfigConstants.java
@@ -0,0 +1,15 @@
+package com.roc.cloud.common.constant;
+
+/**
+ * 配置项常量
+ *
+ * @author: Roc
+ * @date 2019/9/3
+ */
+public interface ConfigConstants {
+    /**
+     * 是否开启自定义隔离规则
+     */
+    String CONFIG_RIBBON_ISOLATION_ENABLED = "xiaogj.x3.ribbon.isolation.enabled";
+
+}
