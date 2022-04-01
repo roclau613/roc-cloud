cd33caffbb29f984c166541351d80ca57af7e6ca
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 20:29:07 +0800

    Create DefaultExceptionAdviceConfig.java

---
+++ roc-cloud-framework-core/src/main/java/com/xiaogj/x3/common/advice/DefaultExceptionAdviceConfig.java
@@ -0,0 +1,37 @@
+package com.roc.cloud.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+/**
+ * 异常通用处理
+ *
+ * @author: Roc
+ */
+@Slf4j
+@Configuration
+public class DefaultExceptionAdviceConfig {
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                //failFast的意思只要出现校验失败的情况，就立即结束校验，不再进行后续的校验。
+                .failFast(true)
+                .buildValidatorFactory();
+        return validatorFactory.getValidator();
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        MethodValidationPostProcessor methodValidationPostProcessor = new MethodValidationPostProcessor();
+        methodValidationPostProcessor.setValidator(validator());
+        return methodValidationPostProcessor;
+    }
+}
