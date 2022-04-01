f9be633397fcab19badf436f358ae3eedaf8b7c1
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 20:38:00 +0800

    Create JsonArrayHandler.java

---
+++ roc-cloud-framework-core/src/main/java/com/xiaogj/x3/common/config/mybatis/type/JsonArrayHandler.java
@@ -0,0 +1,53 @@
+package com.roc.cloud.common.config.mybatis.type;
+
+import com.alibaba.fastjson.JSONArray;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+/**
+ * fastjson转换array <br>
+ *
+ * @date: 2020/11/9 <br>
+ * @author: Roc <br>
+ * @since: 1.0 <br>
+ * @version: 1.0 <br>
+ */
+public class JsonArrayHandler extends BaseTypeHandler<JSONArray> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, JSONArray parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, JSONArray.toJSONString(parameter));
+    }
+
+    @Override
+    public JSONArray getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        String jsonStr = rs.getString(columnName);
+        if (StringUtils.isNotBlank(jsonStr)) {
+            return JSONArray.parseArray(jsonStr);
+        }
+        return null;
+    }
+
+    @Override
+    public JSONArray getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        String jsonStr = rs.getString(columnIndex);
+        if (StringUtils.isNotBlank(jsonStr)) {
+            return JSONArray.parseArray(jsonStr);
+        }
+        return null;
+    }
+
+    @Override
+    public JSONArray getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String jsonStr = cs.getString(columnIndex);
+        if (StringUtils.isNotBlank(jsonStr)) {
+            return JSONArray.parseArray(jsonStr);
+        }
+        return null;
+    }
+}
