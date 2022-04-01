78bb633608c4c4c072943c893738b3ad9bfa8d32
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 20:40:30 +0800

    Create LogLevelConstant.java

---
+++ roc-cloud-framework-core/src/main/java/com/xiaogj/x3/common/constant/LogLevelConstant.java
@@ -0,0 +1,17 @@
+package com.roc.cloud.common.constant;
+
+/**
+ * <br>
+ *
+ * @date: 2021/07/15 <br>
+ * @author: Roc <br>
+ * @since: 1.0 <br>
+ * @version: 1.0 <br>
+ */
+public class LogLevelConstant {
+
+    public final static int WARN = 0;
+
+    public final static int ERROR = 1;
+
+}
