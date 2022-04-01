ec41082c13bfc92f62b73673294c2c2fea7b9ba9
roclau613 <102653448+roclau613@users.noreply.github.com>
2022-04-01 20:33:05 +0800

    Create NotAssemblyBody.java

---
+++ roc-cloud-framework-core/src/main/java/com/xiaogj/x3/common/annotation/NotAssemblyBody.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+package com.roc.cloud.common.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *方法返回不需要装配标注response格式 <br>
+ *
+ * @date: 2020/9/28 <br>
+ * @author: Roc <br>
+ * @since: 1.0 <br>
+ * @version: 1.0 <br>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotAssemblyBody {
+
+    /**
+     * 是否需要匹配Result
+
+     *
+     * @return boolean
+     * @author Roc
+     * @date 2020/9/30
+     **/
+    boolean value() default true;
+}
